a92fc46e9da2e280a6c783b9bba8030e310a4ba3
Shivanand-0 <ishivanandgupta@gmail.com>
2025-05-30 00:53:06 +0530

    updated and removed bugs

--- dental_clinic_chatbot.py
+++ dental_clinic_chatbot.py
@@ -1,6 +1,6 @@
-import re;     
+import re;     # imported re or regEx or reguler expression module to finds exact patterns in strings
 
-
+# response of FAQ 
 faq_responses = {
         1: "We are open from 9 AM to 5 PM, Monday to Saturday.",    #"what are your working hours?"
         2: "We are located at gittikhadan,Nagpur.",     #where are you located?
@@ -10,30 +10,31 @@ faq_responses = {
         6:"Yes, we have a pediatric dentist who specializes in treating children."  #Do you treat kids?
     }
  
+ # chat_processor function: matches pattern and response accordingly
 def chat_processor(command):
-    if re.search(r"\bhello\b|\bhii\b|\bhelp\b|\bgood morning\b|\bhey\b|\bwelcome back\b", command):
+    if re.search(r"\bhello\b|\bhii\b|\bhelp\b|\bgood morning\b|\bhey\b|\bwelcome back\b", command):      #greet
         return "Hello! how can i help you?"
-    elif (re.search(r"(\bwhat\b)",command) and re.search(r"(\bworking hours\b|\bworking time\b|\bhours\b|\btime\b||\bclinic time\b|)",command)):
+    elif re.search(r"\bwhat\b", command) and re.search(r"\b(working hours|working time|hours|time|clinic time)\b", command):
         return faq_responses[1]
-    elif re.search(r"(\bwhat\b|\bwhere\b|)",command) and re.search(r"(\blocated\b|\blocation\b|\boffice\b|\bclinic\b|\bhospital\b)",command):
+    elif re.search(r"\b(what|where)\b",command) and re.search(r"\b(located|location|office|clinic|hospital)\b",command):
         return faq_responses[2]
-    elif re.search(r"(\bwalk-in appointments\b|\bappointments\b)",command):
+    elif re.search(r"\b(walk-in appointments|appointments)\b",command):
         return faq_responses[3]
-    elif re.search(r"(\bwhat\b)",command) and re.search(r"(\bservices do you offer\b|\bservices offer\b|\bservices\b)",command):
+    elif re.search(r"\bwhat\b",command) and re.search(r"\b(services do you offer|services offer|services)\b",command):
         return faq_responses[4]
-    elif re.search(r"(\bhow\b|\bwhat\b)",command) and re.search(r"(\bcontact\b)",command):
+    elif re.search(r"\b(how|what)\b",command) and re.search(r"\bcontact\b",command):
         return faq_responses[5]
-    elif re.search(r"(\btreat kids\b)",command):
+    elif re.search(r"\btreat kids\b",command):
         return faq_responses[6]
     else:
-        return "Sorry, I don’t know that yet."
+        return "Sorry, I don't know that yet."    #not matching responce
 
 
 
 while 1:
-    command= input('you: ' )
-    if re.search(r"\bbye\b|\bexit\b|\bterminate\b|\bquit\b", command):
-        print("Bot: Good Bye!")
+    command= input('you: ' )    #user input or command or question
+    if re.search(r"\bbye\b|\bexit\b|\bterminate\b|\bquit\b", command):    
+        print("Bot: Good Bye!")                                       #responce of termination 
         break;
-    response= chat_processor(command.lower());
+    response= chat_processor(command.lower());   #handling responce for patient
     print("Bot: ",response)
